5105cc943a231b0ef8c139ef2891efcc5948bb78
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-09-09 23:53:22 -0300

    new updates in App.tsx.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar
+} from 'react-native';
+
+const isAndroid = Platform.OS == 'android';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <ExpoStatusBar style="auto" />
+
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text style={styles.text}>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.text}>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
   },
+  search: {
+    backgroundColor: 'green',
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '600'
+  }
 });
