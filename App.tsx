8905874184ff4881dc3c5b904dfeb7f89632bd38
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-09-12 00:12:29 -0300

    Added RestaurantsScreen.

--- App.tsx
+++ App.tsx
@@ -1,49 +1,12 @@
 import * as React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from "react-native";
-
-const isAndroid = Platform.OS === "android";
+import { RestaurantsScreen } from "./src/screens/RestaurantsScreen";
 
 export default function App() {
   return (
     <>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
-
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text style={styles.text}>Search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text style={styles.text}>List</Text>
-        </View>
-      </SafeAreaView>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    backgroundColor: "green",
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: "600",
-  },
-});
