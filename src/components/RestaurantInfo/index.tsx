c29245866c8fe76d673d82912b33bc90f9afddbc
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-09-12 21:41:46 -0300

    Added some tests with react-native-paper card.

--- src/components/RestaurantInfo/index.tsx
+++ src/components/RestaurantInfo/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
-export default function RestaurantInfo() {
+// type Props = {
+//   name: string;
+//   icon: string;
+//   photos: [];
+//   address: string;
+//   isOpenNow: boolean;
+//   rating: number;
+//   isClosedTemporarily: boolean;
+// };
+
+export function RestaurantInfoCard({ restaurant = {} }) {
+  const { name, photos } = restaurant;
+
   return (
-    <View>
-      <Text>RestaurantInfo</Text>
-    </View>
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
--- src/screens/RestaurantsScreen/index.tsx
+++ src/screens/RestaurantsScreen/index.tsx
@@ -2,16 +2,29 @@ import * as React from "react";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
 
-import RestaurantInfo from "../../components/RestaurantInfo";
+import { RestaurantInfoCard } from "../../components/RestaurantInfo";
 
 export function RestaurantsScreen() {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const mockData = {
+    name: "Some Restaurant",
+    icon: "",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.search}>
-        <Searchbar />
+        <Searchbar onChangeText={setSearchQuery} value={searchQuery} />
       </View>
       <View style={styles.list}>
-        <RestaurantInfo />
+        <RestaurantInfoCard restaurant={mockData} />
       </View>
     </SafeAreaView>
   );
@@ -28,6 +41,6 @@ const styles = StyleSheet.create({
   list: {
     flex: 1,
     padding: 16,
-    backgroundColor: "blue",
+    backgroundColor: "#0840b9",
   },
 });
