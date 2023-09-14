21229a5652d6d1c8d7c1d45b47fde2141ed0447b
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-09-13 21:35:16 -0300

    Created some styles and theme.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/screens/RestaurantsScreen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/styles/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
