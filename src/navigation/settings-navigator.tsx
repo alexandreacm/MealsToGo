a21a49a18619b53d74265af6dbbc82409ab24f14
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-02 15:52:33 -0300

    211.Favorites Screen Exercise.

--- src/navigation/settings-navigator.tsx
+++ src/navigation/settings-navigator.tsx
@@ -4,6 +4,7 @@ import {
   CardStyleInterpolators,
 } from "@react-navigation/stack";
 import { SettingsScreen } from "../screens/SettingsScreen";
+import { FavoritesScreen } from "../screens/FavoritesScreen";
 
 const SettingsStack = createStackNavigator();
 
@@ -22,7 +23,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         name="Settings"
         component={SettingsScreen}
       />
-      <SettingsStack.Screen name="Favorites" component={() => null} />
+      <SettingsStack.Screen name="Favorites" component={FavoritesScreen} />
     </SettingsStack.Navigator>
   );
 };
