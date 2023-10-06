e0f7ecc24e7b1ab6f0a1f9b2d659df764a464d53
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-06 00:43:39 -0300

    Some improvements in restaurant-detail-screen.

--- src/screens/RestaurantDetailScreen/index.tsx
+++ src/screens/RestaurantDetailScreen/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ScrollView } from "react-native";
-import { List } from "react-native-paper";
+import { List, Divider } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../../components/RestaurantInfoCard";
 import { SafeArea } from "../../utility/SafeArea";
@@ -34,9 +34,11 @@ export const RestaurantDetailScreen = ({ route }: RouteParams) => {
           onPress={() => setBreakfastExpanded(!breakfastExpanded)}
         >
           <List.Item title="Eggs Benedict" />
+          <Divider />
           <List.Item title="Classic Breakfast" />
         </List.Accordion>
 
+        <Divider />
         <List.Accordion
           title="Lunch"
           left={(props) => <List.Icon {...props} icon="hamburger" />}
@@ -44,10 +46,14 @@ export const RestaurantDetailScreen = ({ route }: RouteParams) => {
           onPress={() => setLunchExpanded(!lunchExpanded)}
         >
           <List.Item title="Burger w/ Fries" />
+          <Divider />
           <List.Item title="Steak Sandwich" />
+          <Divider />
           <List.Item title="Mushroom Soup" />
+          <Divider />
         </List.Accordion>
 
+        <Divider />
         <List.Accordion
           title="Dinner"
           left={(props) => <List.Icon {...props} icon="food-variant" />}
@@ -55,9 +61,13 @@ export const RestaurantDetailScreen = ({ route }: RouteParams) => {
           onPress={() => setDinnerExpanded(!dinnerExpanded)}
         >
           <List.Item title="Spaghetti Bolognese" />
+          <Divider />
           <List.Item title="Veal Cutlet with Chicken Mushroom Rotini" />
+          <Divider />
           <List.Item title="Steak Frites" />
         </List.Accordion>
+
+        <Divider />
 
         <List.Accordion
           title="Drinks"
@@ -66,9 +76,13 @@ export const RestaurantDetailScreen = ({ route }: RouteParams) => {
           onPress={() => setDrinksExpanded(!drinksExpanded)}
         >
           <List.Item title="Coffee" />
+          <Divider />
           <List.Item title="Tea" />
+          <Divider />
           <List.Item title="Modelo" />
+          <Divider />
           <List.Item title="Coke" />
+          <Divider />
           <List.Item title="Fanta" />
         </List.Accordion>
       </ScrollView>
