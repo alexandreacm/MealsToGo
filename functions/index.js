beeeb217990ef30f6cac2a3ff1f3a55b266d1078
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-07 14:21:33 -0300

    Moving Restaurants mock.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const { geocodeRequest } = require("./geocode");
+const { placesRequest } = require("./places");
 
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
@@ -13,4 +14,8 @@ exports.helloWorld = onRequest((request, response) => {
 
 exports.geocode = onRequest((request, response) => {
   geocodeRequest(request, response);
+});
+
+exports.placesNearby = onRequest((request, response) => {
+  placesRequest(request, response);
 });
--- src/services/restaurants/restaurants.service.ts
+++ src/services/restaurants/restaurants.service.ts
@@ -1,22 +1,26 @@
-import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
+// import { mocks, mockImages } from "../../../functions/places/mock";
+
+// export const restaurantsRequest = (location) => {
+//   return new Promise((resolve, reject) => {
+//     const mock = mocks[location];
+//     if (!mock) {
+//       reject("not found");
+//     }
+//     resolve(mock);
+//   });
+// };
 
 export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
+  return fetch(
+    `http://localhost:5001/mealstogo-92344/us-central1/placesNearby?location=${location}`,
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
     return {
       ...restaurant,
       address: restaurant.vicinity,
@@ -27,3 +31,20 @@ export const restaurantsTransform = ({ results = [] }) => {
 
   return camelize(mappedResults);
 };
+
+// export const restaurantsTransform = ({ results = [] }) => {
+//   const mappedResults = results.map((restaurant) => {
+//     restaurant.photos = restaurant.photos.map((p) => {
+//       return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+//     });
+
+//     return {
+//       ...restaurant,
+//       address: restaurant.vicinity,
+//       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+//       isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+//     };
+//   });
+
+//   return camelize(mappedResults);
+// };
