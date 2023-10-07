541e94614af676e22b433ac77cc234ec205ee976
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-07 16:54:01 -0300

    Added .env file and .env-example.

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,27 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
 
-  response.json(locationMock);
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: process.env.GOOGLE_API_KEY,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,20 +2,24 @@ const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
 const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
 
 exports.geocode = onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
