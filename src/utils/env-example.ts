02af6ccb9f4da911365f857c6fe3ef550923f6e6
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-11 09:55:57 -0300

    Updated mu env-example file.

--- src/utils/env-example.ts
+++ src/utils/env-example.ts
@@ -1,10 +1,14 @@
+import { Platform } from "react-native";
+
 const liveHost = "XXX";
 const localHost = "http://localhost:5001/mealstogo-92344/us-central1";
 
+export const isAndroid = Platform.OS === "android";
+
 export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const host = isDevelopment ? localHost : liveHost;
+export const host = isDevelopment || isAndroid ? liveHost : localHost;
 
-export const isMock = false;
+export const isMock = true;
 
 export const tokenStripe = "XXX";
